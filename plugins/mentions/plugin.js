c5ebfe083cc9d7d0d53e2e7b958cda5ff6932eb1
pairing <pairing@ackbar.ca.com>
2017-02-21 11:26:08 -0700

    WIP refactor

--- plugins/mentions/plugin.js
+++ plugins/mentions/plugin.js
@@ -1,4 +1,13 @@
 (function() {
+
+  var users = [
+    { name: 'T-roy' },
+    { name: '"T124"' },
+    { name: '<ref>' },
+      { name: 'this1isjustaverylongnametotesthowitcanfitthesuggestionsdropdownhadanyoneusedacrazylongnamelikethisthoughunlikely' },
+      { name: 'namewitha space' }
+    ];
+
   function cleanup(editorInstance) {
     if (!editorInstance.isMentioning) {
       return;
@@ -18,6 +27,97 @@
     editorInstance.isMentioning = false;
   }
 
+  function formatName(name) {
+    return name.replace(/[<>&\n]/g, function(x) {
+      return {
+        '<': '&lt;',
+        '>': '&gt;',
+        '&': '&amp;',
+        '\n': '<br />'
+      }[x];
+    });
+  }
+
+  function startMentioningKeyEvent(editorInstance, event) {
+    if (!editorInstance.isMentioning && event.data.keyCode === CKEDITOR.SHIFT + 50) { // @
+      event.cancel();
+      var mentionSpan = editorInstance.mentionSpan = editorInstance.document.createElement('span', {
+        attributes: {
+          Class: 'mention'
+        }
+      });
+      editorInstance.insertElement(mentionSpan);
+
+      var range = editorInstance.createRange();
+      range.moveToElementEditablePosition(mentionSpan, true);
+      editorInstance.getSelection().selectRanges([range]);
+
+      editorInstance.insertText('@');
+
+      editorInstance.isMentioning = true;
+
+      if (!editorInstance.mentionList) {
+        var mentionSpan = editorInstance.mentionSpan;
+        editorInstance.mentionList = CKEDITOR.dom.element.createFromHtml('<div class="mention-list"></div>');
+        mentionSpan.append(editorInstance.mentionList);
+        editorInstance.mentionSpan.removeAttribute('data-uuid');
+      }
+
+      var suggestions = users.map(function(mention, index) {
+        var selectedClass = index === 0 ? 'selected' : '';
+        return '<div class="' + selectedClass + '" data-uuid="' + 'example' + '"data-id="mention-item"' + '>' + formatName(mention.name) + '</div>';
+      });
+
+      editorInstance.mentionList.setHtml(suggestions.join(''));
+    }
+  }
+
+  function moveSelectTo(event, whichSibling) {
+    event.cancel();
+    var editorInstance = event.editor;
+    var selected = editorInstance.mentionList.$.querySelector('.selected');
+    var nextSelected = selected[whichSibling]
+    if (editorInstance.mentionList) {
+      if (selected && nextSelected) {
+        selected.className = '';
+        nextSelected.className = 'selected';
+        editorInstance.mentionList.$.scrollTop = nextSelected.offsetTop;
+      }
+    }
+  }
+
+  function keyboardInterationKeyEvents(editorInstance, event) {
+    if (editorInstance.isMentioning) {
+      if (event.data.keyCode === 13) { //enter
+        event.cancel();
+
+        var selected = editorInstance.mentionList.$.querySelector('.selected');
+        var uuid = selected.getAttribute('data-uuid');
+        var name = selected.textContent;
+
+        if (uuid) {
+          editorInstance.mentionSpan.setAttribute('data-uuid', uuid);
+          editorInstance.mentionSpan.setText('@' + name);
+
+          editorInstance.mentionList.remove();
+
+          var range = editorInstance.createRange();
+          range.moveToPosition(editorInstance.mentionSpan, CKEDITOR.POSITION_AFTER_END);
+          editorInstance.getSelection().selectRanges([range]);
+          editorInstance.insertText(' ');
+
+          cleanup(editorInstance);
+        }
+      } else if (event.data.keyCode === 27) { // ESC
+        cleanup(editorInstance);
+      } else if (event.data.keyCode === 38) { // Up
+        moveSelectTo(event, 'previousSibling');
+      } else if (event.data.keyCode === 40) { // Down
+        moveSelectTo(event, 'nextSibling');
+      }
+    }
+  }
+
   CKEDITOR.plugins.add('mentions', {
     init: function(editor) {
 
@@ -25,104 +125,11 @@
         event.editor.document.appendStyleSheet(CKEDITOR.plugins.getPath('mentions') + 'mentions.css');
       });
 
-      var users = [
-        { name: 'T-roy' },
-        { name: '"T124"' },
-        { name: '<ref>' },
-        { name: 'this1isjustaverylongnametotesthowitcanfitthesuggestionsdropdownhadanyoneusedacrazylongnamelikethisthoughunlikely' },
-        { name: 'namewitha space' }
-      ];
-
       editor.on('key', function(event) {
         var editorInstance = event.editor;
-        if (!editorInstance.isMentioning && event.data.keyCode === CKEDITOR.SHIFT + 50) { // @
-          event.cancel();
-          var mentionSpan = editorInstance.mentionSpan = editorInstance.document.createElement('span', {
-            attributes: {
-              Class: 'mention'
-            }
-          });
-          editorInstance.insertElement(mentionSpan);
 
-          var range = editorInstance.createRange();
-          range.moveToElementEditablePosition(mentionSpan, true);
-          editorInstance.getSelection().selectRanges([range]);
-
-          editorInstance.insertText('@');
-
-          editorInstance.isMentioning = true;
-
-          if (!editorInstance.mentionList) {
-            var mentionSpan = editorInstance.mentionSpan;
-            editorInstance.mentionList = CKEDITOR.dom.element.createFromHtml('<div class="mention-list"></div>');
-            mentionSpan.append(editorInstance.mentionList);
-            editorInstance.mentionSpan.removeAttribute('data-uuid');
-          }
-
-          function formatName(name) {
-            return name.replace(/[<>&\n]/g, function(x) {
-              return {
-                '<': '&lt;',
-                '>': '&gt;',
-                '&': '&amp;',
-                '\n': '<br />'
-              }[x];
-            });
-          }
-
-          var suggestions = users.map(function(mention, index) {
-            var selectedClass = index === 0 ? 'selected' : '';
-            return '<div class="' + selectedClass + '" data-uuid="' + 'example' + '"data-id="mention-item"' + '>' + formatName(mention.name) + '</div>';
-          });
-
-          editorInstance.mentionList.setHtml(suggestions.join(''));
-        }
-
-        if (editorInstance.isMentioning) {
-          if (event.data.keyCode === 13) { //enter
-            event.cancel();
-
-            var selected = editorInstance.mentionList.$.querySelector('.selected');
-            var uuid = selected.getAttribute('data-uuid');
-            var name = selected.textContent;
-
-            if (uuid) {
-              editorInstance.mentionSpan.setAttribute('data-uuid', uuid);
-              editorInstance.mentionSpan.setText('@' + name);
-
-              editorInstance.mentionList.remove();
-
-              var range = editorInstance.createRange();
-              range.moveToPosition(editorInstance.mentionSpan, CKEDITOR.POSITION_AFTER_END);
-              editorInstance.getSelection().selectRanges([range]);
-              editorInstance.insertText(' ');
-
-              cleanup(editorInstance);
-            }
-          } else if (event.data.keyCode === 27) { // ESC
-            cleanup(editorInstance);
-          } else if (event.data.keyCode === 38) { // Up
-            if (editorInstance.mentionList) {
-              event.cancel();
-              var selected = editorInstance.mentionList.$.querySelector('.selected');
-              if (selected && selected.previousSibling) {
-                selected.className = '';
-                selected.previousSibling.className = 'selected';
-                editorInstance.mentionList.$.scrollTop = selected.previousSibling.offsetTop;
-              }
-            }
-          } else if (event.data.keyCode === 40) { // Down
-            if (editorInstance.mentionList) {
-              event.cancel();
-              var selected = editorInstance.mentionList.$.querySelector('.selected');
-              if (selected && selected.nextSibling) {
-                selected.className = '';
-                selected.nextSibling.className = 'selected';
-                editorInstance.mentionList.$.scrollTop = selected.offsetTop;
-              }
-            }
-          }
-        }
+        startMentioningKeyEvent(editorInstance, event);
+        keyboardInterationKeyEvents(editorInstance, event);
       });
 
       editor.on('blur', function(event) {
