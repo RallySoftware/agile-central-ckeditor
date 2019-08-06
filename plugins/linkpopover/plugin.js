c964cbf9c3642058c924e1579fa8e600795eb9fd
Cassidy Thompson <cassdamthompson@gmail.com>
2019-08-06 10:33:42 -0600

    reverting master to 0.7.13

--- plugins/linkpopover/plugin.js
+++ plugins/linkpopover/plugin.js
@@ -58,13 +58,11 @@
 	var repositionPopover = function(editor, link) {
 		var popover = editor.popover;
 		var contentElement = getContentElement(editor);
-		
-		var adjustedOffsetTop = link.$.offsetTop - editor.window.$.pageYOffset;
 
 		popover.removeStyle("right");
 
 		popover.setStyle("left", link.$.offsetLeft + "px");
-		popover.setStyle("top", (adjustedOffsetTop + link.$.offsetHeight - editor.editable().$.scrollTop) + "px");
+		popover.setStyle("top", (link.$.offsetTop + link.$.offsetHeight - editor.editable().$.scrollTop) + "px");
 
 		// Will the popover overflow the area
 		if (popover.$.offsetLeft + popover.$.offsetWidth > contentElement.$.offsetWidth) {
@@ -72,7 +70,7 @@
 			popover.setStyle("right", "5px");
 		}
 		if (popover.$.offsetTop + popover.$.offsetHeight > contentElement.$.offsetHeight) {
-			popover.setStyle("top", (adjustedOffsetTop - editor.editable().$.scrollTop - popover.$.offsetHeight - 5) + "px");
+			popover.setStyle("top", (link.$.offsetTop - editor.editable().$.scrollTop - popover.$.offsetHeight - 5) + "px");
 		}
 		popover.setStyle("max-width", (contentElement.$.offsetWidth - 10) + "px");
 	};
